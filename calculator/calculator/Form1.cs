fc1cea76e454ff54ff18ccfbde474b9807538940
vkuth <vkuth@mail.ru>
2019-07-16 12:54:16 +0600

    add +-*/

--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -16,5 +16,39 @@ namespace calculator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tb1 = Convert.ToDouble(textBox1.Text);
+            double tb2 = Convert.ToDouble(textBox2.Text);
+            double tb3 = tb1 + tb2;
+            textBox3.Text = tb3.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double tb1 = Convert.ToDouble(textBox1.Text);
+            double tb2 = Convert.ToDouble(textBox2.Text);
+            double tb3 = tb1 - tb2;
+            textBox3.Text = tb3.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double tb1 = Convert.ToDouble(textBox1.Text);
+            double tb2 = Convert.ToDouble(textBox2.Text);
+            double tb3 = tb1 * tb2;
+            textBox3.Text = tb3.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double tb1 = Convert.ToDouble(textBox1.Text);
+            double tb2 = Convert.ToDouble(textBox2.Text);
+            double tb3 = tb1 / tb2;
+            textBox3.Text = tb3.ToString();
+        }
+
+       
     }
 }
