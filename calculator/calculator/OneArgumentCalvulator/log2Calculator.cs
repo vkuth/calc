3805dc9c333d2f66b9bc25ad4c27554e8cf5f7cd
vkuth <vkuth@mail.ru>
2019-07-18 12:18:18 +0600

    Create log2Calculator.cs

---
+++ calculator/calculator/OneArgumentCalvulator/log2Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.OneArgumentCalvulator
+{
+    class log2Calculator : IOneArgumentCalculator
+    {
+        public double Calculate(double firstValue)
+        {
+            return Math.Log(firstValue,2);
+        }
+
+    }
+}
