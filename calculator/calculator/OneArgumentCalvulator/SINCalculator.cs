c29b6de52b3805973bceaea11bbad1bfdef07abf
kirill88888 <steijerr@gmail.com>
2019-07-18 11:56:49 +0600

    rename solution

--- calculator/calculator/OneArgumentCalvulator/SINCalculator.cs
+++ calculator/calculator/OneArgumentCalvulator/SINCalculator.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace calculator
+namespace calculator.OneArgumentCalvulator
 {
     class SINCalculator: IOneArgumentCalculator
     {
--- calculator/calculator/TwoArgumentCalculator/CalculatorFactory.cs
+++ calculator/calculator/TwoArgumentCalculator/CalculatorFactory.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace calculator
+namespace calculator.TwoArgumentCalculator
 {
     public static class CalculatorFactory
     {
