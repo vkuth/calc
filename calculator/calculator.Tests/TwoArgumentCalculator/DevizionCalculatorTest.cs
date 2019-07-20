d3a43bdda0060b05a45fd0958941a296830b477b
kirill88888 <vkuth@mail.ru>
2019-07-20 13:45:09 +0600

    Devizion exception

--- calculator/calculator.Tests/TwoArgumentCalculator/DevizionCalculatorTest.cs
+++ calculator/calculator.Tests/TwoArgumentCalculator/DevizionCalculatorTest.cs
@@ -1,4 +1,5 @@
-﻿using calculator.TwoArgumentCalculator;
+﻿using System;
+using calculator.TwoArgumentCalculator;
 using NUnit.Framework;
 
 namespace calculator.Tests.TwoArgumentCalculator
@@ -12,6 +13,14 @@ namespace calculator.Tests.TwoArgumentCalculator
             ICalculator calculator = new divizioncalculator();
             double result = calculator.Calculate(6, 2);
             Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void DevizionByZeroTest()
+        {
+            ICalculator calculator = new divizioncalculator();
+            Assert.Throws<Exception>(() => calculator.Calculate(6, 0));
+
         }
     }
 }
--- calculator/calculator/TwoArgumentCalculator/delitcalculator.cs
+++ calculator/calculator/TwoArgumentCalculator/delitcalculator.cs
@@ -1,9 +1,16 @@
-﻿namespace calculator.TwoArgumentCalculator
+﻿using System;
+
+namespace calculator.TwoArgumentCalculator
 {
      public class divizioncalculator : ICalculator
     {
         public double Calculate(double firstvalue, double secondvalue)
         {
+            if (secondvalue == 0)
+            {
+                throw new Exception("Деление на 0");
+            }
+
             return firstvalue / secondvalue;
         }
     }
