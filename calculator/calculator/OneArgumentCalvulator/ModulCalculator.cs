26de0f34f9755cf5eda73a90381f3f7a3c406e38
kirill88888 <vkuth@mail.ru>
2019-07-20 11:38:30 +0600

    modul test

--- calculator/calculator/OneArgumentCalvulator/ModulCalculator.cs
+++ calculator/calculator/OneArgumentCalvulator/ModulCalculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace calculator.OneArgumentCalvulator
 {
-    class ModulCalculator : IOneArgumentCalculator
+    public class ModulCalculator : IOneArgumentCalculator
     {
         public double Calculate(double firstValue)
         {
