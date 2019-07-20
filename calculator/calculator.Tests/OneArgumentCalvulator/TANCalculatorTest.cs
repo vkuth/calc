56ac1c0b798f6d12342453e7937697a876fab94a
kirill88888 <vkuth@mail.ru>
2019-07-20 11:30:31 +0600

    tan test

---
+++ calculator/calculator.Tests/OneArgumentCalvulator/TANCalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using calculator.OneArgumentCalvulator;
+using NUnit.Framework;
+
+namespace calculator.Tests.OneArgumentCalvulator
+{
+    [TestFixture]
+    public class TANCalculatorTest
+    {
+        [TestCase(1, 1.5574)]
+        [TestCase(3, -0.1425)]
+        [TestCase(7, 0.8714)]
+        public void CalculateTest(double firstValue, double expected)
+        {
+            var calculator = new TANCalculator();
+            var result = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, result, 0.0001);
+        }
+    }
+}
--- calculator/calculator/OneArgumentCalvulator/TANCalculator.cs
+++ calculator/calculator/OneArgumentCalvulator/TANCalculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace calculator.OneArgumentCalvulator
 {
-    class TANCalculator : IOneArgumentCalculator
+    public class TANCalculator : IOneArgumentCalculator
     {
         public double Calculate(double firstValue)
         {
