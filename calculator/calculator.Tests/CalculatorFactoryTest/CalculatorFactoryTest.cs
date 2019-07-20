176f3796465de702d016070c4c8819b552a821a3
kirill88888 <vkuth@mail.ru>
2019-07-20 12:19:53 +0600

    factory test

---
+++ calculator/calculator.Tests/CalculatorFactoryTest/CalculatorFactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using calculator.TwoArgumentCalculator;
+using NUnit.Framework;
+
+namespace calculator.Tests.CalculatorFactoryTest
+{
+    [TestFixture]
+    public class CalculatorFactoryTest
+    {
+        [TestCase("plus", typeof(PlusCalculator))]
+        [TestCase("minus", typeof(MinusCalculator))]
+        [TestCase("multipli", typeof(multiplicalculator))]
+        [TestCase("divizion", typeof(divizioncalculator))]
+        [TestCase("frdegree", typeof(FractionalDegreeCalculator))]
+        [TestCase("logxy", typeof(logxyCalculator))]
+        [TestCase("max", typeof(maxCalculator))]
+        [TestCase("arif", typeof(MiddleArifmetCalculator))]
+        [TestCase("mod", typeof(ModCalculator))]
+        [TestCase("xony", typeof(xonyCalculator))]
+        public void CreateCalculatorTest(string name, Type type)
+        {
+            var calculator = CalculatorFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
