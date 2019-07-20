14dbf0c2170daa89edaef0fc585bead35be75c59
kirill88888 <vkuth@mail.ru>
2019-07-20 11:32:58 +0600

    anti test

---
+++ calculator/calculator.Tests/OneArgumentCalvulator/AntiCalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using calculator.OneArgumentCalvulator;
+using NUnit.Framework;
+
+namespace calculator.Tests.OneArgumentCalvulator
+{
+    [TestFixture]
+    public class AntiCalculatorTest
+    {
+        [TestCase(1, -1)]
+        [TestCase(3, -3)]
+        [TestCase(-7, 7)]
+        public void CalculateTest(double firstValue, double expected)
+        {
+            var calculator = new AntiCalculator();
+            var result = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, result, 0.0001);
+        }
+    }
+}
--- calculator/calculator/OneArgumentCalvulator/AntiCalculator.cs
+++ calculator/calculator/OneArgumentCalvulator/AntiCalculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace calculator.OneArgumentCalvulator
 {
-    class AntiCalculator : IOneArgumentCalculator
+    public class AntiCalculator : IOneArgumentCalculator
     {
         public double Calculate(double firstValue)
         {
